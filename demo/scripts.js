8db3066f8ee2df1789479d34d507d5329653f159
krdatalab <282023809+krdatalab@users.noreply.github.com>
2026-07-03 00:16:55 +0000

    x

---
+++ demo/scripts.js
@@ -0,0 +1,21 @@
+const Pet {
+    image: "",
+    name: "",
+    age: 0
+    breed: "",
+
+}
+
+function createPet(image, name, age, breed, description, isAdopted, favorite) {
+    
+}
+
+const mochiPet = createPet(./image.png, )
+
+function createPetCard(pet) {
+    petContainer.innerHTML = pet;
+}
+
+createPetCard
+
+document.getElement ByID
